06240990a46f953ddaddb7bc85e1063d5c91c360
JeevesGB <jeeveslord@gmail.com>
2025-09-23 14:14:32 +0100

    fixed bat file

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Build game into a single EXE with icon
-pyinstaller --onefile --windowed --icon=thegrin.ico --name Click The Twing game.py
+pyinstaller --onefile --windowed --icon=thegrin.ico --name ClickTheTwing game.py
 
 REM Move EXE to a clean "dist" folder
 echo.
